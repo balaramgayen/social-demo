d38e640d751ae77a9ac6c0a15f45a3ed0ae2e5ae
Balaram Gayen <balaramgayen53501@gmail.com>
2022-08-22 17:44:30 +0530

    first relase to heroku

---
+++ index.js
@@ -0,0 +1,28 @@
+const express = require("express");
+
+const app = express();
+
+const PORT = 4000;
+
+app.get("/", (re, res) => {
+  res.status(200).send("<h1>Backend Running</h1>");
+});
+
+app.get("/api/v1/facebook", (req, res) => {
+  const data = {
+    username: "Balaram53",
+    followers: 444,
+    follows: 411,
+    date: Date.now(),
+  };
+
+  res.status(200).json({ data });
+});
+
+app.get("/api/v1/:name", (req, res) => {
+  res.status(200).json({ parameter: req.params.name });
+});
+
+app.listen(PORT, () => {
+  console.log(`Backend running on port ${PORT} cheers`);
+});
